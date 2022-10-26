de6795e1bc216bc320171043d7d1bdfa202d1f18
jinhui18 <JINHUICHEONG9@GMAIL.COM>
2022-10-26 20:46:18 +0800

    26/10/22 8.45pm lots of changes

---
+++ app/src/main/java/com/example/application/CreateNewAccountVerifyEmail.java
@@ -0,0 +1,34 @@
+package com.example.application;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateNewAccountVerifyEmail extends AppCompatActivity {
+
+    private TextView textView1, textView2, textView3;
+    private Button loginButton;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_new_account_verify_email);
+
+        loginButton = findViewById(R.id.button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CreateNewAccountVerifyEmail.this, MainActivity.class));
+            }
+        });
+
+    }
+
+
+
+}
