7d099f046eed58cb452e51aa36a944da9a459ef1
Isaachzj <IHONG001@e.ntu.edu.sg>
2022-10-31 23:48:32 +0800

    Deleted email and password in acount

--- app/src/main/java/com/example/application/backend/entity/Account.java
+++ app/src/main/java/com/example/application/backend/entity/Account.java
@@ -8,27 +8,16 @@ public class Account {
         private String name;
 
         /**
-         * The email address of the user.
+         * The profile of the user
          */
-        protected String email;
-
-        /**
-         * The password to the user's account.
-         */
-        protected String password;
-
         private Profile profile;
 
-
-
         public Account () {
-
         }
 
-        public Account(String name,  String email, String password, Profile profile) {
+        public Account(String name,  Profile profile) {
             this.name = name;
-            this.email = email;
-            this.password = password;
+            this.profile = profile;
         }
 
         /**
@@ -47,40 +36,4 @@ public class Account {
         public String getName() {
             return this.name;
         }
-
-
-        /**
-         * Gets the login email address of the user.
-         * @return	this user's login email address
-         */
-        public String getEmail() {
-            return email;
-        }
-
-        /**
-         * Sets the login email address of the user.
-         * @param email	This user's login email address
-         */
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        /**
-         * Gets the password to the user's account.
-         * @return	this user's account password
-         */
-        public String getPassword() {
-            return password;
-        }
-
-        /**
-         * Sets the password to the user's account.
-         * @param password	This user's account password
-         */
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-
-
 }
