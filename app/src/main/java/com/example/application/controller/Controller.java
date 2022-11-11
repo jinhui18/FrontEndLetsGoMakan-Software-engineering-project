8655901def492ed719d25a93d62eab93adb0b4f1
Isaachzj <IHONG001@e.ntu.edu.sg>
2022-11-12 01:13:11 +0800

    More javadocs

--- app/src/main/java/com/example/application/controller/Controller.java
+++ app/src/main/java/com/example/application/controller/Controller.java
@@ -6,14 +6,35 @@ import com.example.application.model.Model;
 
 import java.util.ArrayList;
 
+/**
+ * Controller class, part of the MVC architecture, captures user input from the View classes (UI display activity classes) and handles these events
+ * It handles these events through its handleEvent()
+ * @author Isaac
+ * @version 1.0
+ * @since 2022-11-10
+ */
 public class Controller {
+    /**
+     * refers to the Model object in MVC architecture that contains the service logic to modify the database in response to the event
+     */
    protected Model model;
+    /**
+     * This is the arraylist that stores any parameters needed to perform the service logic of the Model class
+     */
    protected ArrayList<Object> list;
 
+    /**
+     * This is the overridden controller class constructor
+     * @param model is the model object
+     * @param list is the arraylist of parameters needed by the Model object in its service() method
+     */
     public Controller(Model model, ArrayList<Object> list){
         this.model = model; this.list = list;
     }
 
+    /**
+     * This is the method that handles any event from the View classes
+     */
     public void handleEvent(){
         model.addAttributeList(list);
         model.service();
--- app/src/main/java/com/example/application/model/Model.java
+++ app/src/main/java/com/example/application/model/Model.java
@@ -8,15 +8,49 @@ import com.google.firebase.database.DatabaseReference;
 import java.util.ArrayList;
 import java.util.Observable;
 
+/**
+ * This is the abstract Model class in the MVC architecture that other child model classes will inherit from
+ * The model child classes will implement the service() method with their respective service logic
+ * @author Isaac
+ * @version 1.0
+ * @since 2022-11-10
+ */
 public abstract class Model extends Observable{
+    /**
+     * mAuth is the reference to our Firebase Authenticator
+     */
     protected FirebaseAuth mAuth;
+    /**
+     * mDatabase is a reference to our Firebase realtime database
+     */
     protected DatabaseReference mDatabase;
+    /**
+     * context refers to the UI activity page in which the event had occurred (View class)
+     */
     protected Context context;
+    /**
+     * attribute list is the arraylist that refers to the list of parameters needed to perform the service logic in the child classes overridden service() method
+     */
     protected ArrayList<Object> attributeList;
 
+    /**
+     *
+     * @param mAuth is the reference to our Firebase Authenticator
+     * @param mDatabase is a reference to our Firebase realtime database
+     * @param context refers to the UI activity page in which the event had occurred (View class)
+     */
     public Model(FirebaseAuth mAuth, DatabaseReference mDatabase, Context context){
         this.mAuth=mAuth; this.mDatabase=mDatabase; this.context=context; this.attributeList=null;
     }
+
+    /**
+     * The abstract service() method that individual model child classes will inherit and implement
+     */
     public abstract void service();
+
+    /**
+     * get method to add attribute list to the model object
+     * @param list refers to the attribute list to be added
+     */
     public void addAttributeList(ArrayList<Object> list) {attributeList=list;}
 }
