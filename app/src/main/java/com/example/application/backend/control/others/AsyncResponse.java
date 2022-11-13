1f7fa27910773581637839f629983f16e4e7c7b8
jinhui18 <JINHUICHEONG9@GMAIL.COM>
2022-11-13 20:54:41 +0800

    JAVA DOCS DONE
    FINAL STATIC VERSION == TRUE

--- app/src/main/java/com/example/application/backend/control/others/AsyncResponse.java
+++ app/src/main/java/com/example/application/backend/control/others/AsyncResponse.java
@@ -4,6 +4,13 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import java.util.ArrayList;
 
+
+/**
+ * This class is to provide an interface to get the results from all the APIs in the FirebaseForAPI class.
+ * @author Pratham
+ * @version 1.0
+ * @since 2022-11-11
+ */
 public interface AsyncResponse {
 
     ////Place Details
